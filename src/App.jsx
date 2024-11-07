e28668460866530692e95719854518d29929b20f
Andres Ruiz Flores <andresrf74@gmail.com>
2024-11-06 21:07:38 -0300

    Creando Proyecto Parcial

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+
+import './App.css'
+import Card from './components/Card'
+
+function App() {
+  //const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <div className='App'>
+        <h1>Carga de estudiantes</h1>
+        <form></form>
+        <Card />
+      </div>
+      
+    </>
+  )
+}
+
+export default App
